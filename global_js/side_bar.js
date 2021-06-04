50fa33f1cb45b90f2f3ddf106e107db03bfe4f50
MiDe-S <63002269+MiDe-S@users.noreply.github.com>
2021-06-04 14:21:07 -0400

    made button opaque
    also moved index link to "done section" on main index.html

--- global_js/side_bar.js
+++ global_js/side_bar.js
@@ -43,6 +43,7 @@ function openNav() {
     else {
         document.getElementById("mySidebar").style.width = "250px";
         document.getElementById("main").style.marginLeft = "250px";
+        document.getElementById("nav_button").style.opacity = '1.0';
     }
 
 }
@@ -50,4 +51,5 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("nav_button").style.opacity = '.35';
 }
